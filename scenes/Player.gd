b419820dab34f20641e08564e4f61960a5eb49fb
Lenny Critchley <LennyPhoenixC@gmail.com>
2021-11-04 22:27:07 +0000

    :sparkles: Add Player sneaking

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
-export var move_speed := 100.0
+export var move_speed := 80.0
 export var acceleration := 1500.0
 export var drag := 1000.0
+export var sneaking_speed_multiplier := 0.4
 
+var sneaking := false
 var input_vector := Vector2.ZERO
 var velocity := Vector2.ZERO
 
@@ -18,9 +20,12 @@ func _process(_delta: float) -> void:
 	input_vector.y -= Input.get_action_strength("up")
 	input_vector.y += Input.get_action_strength("down")
 	input_vector = input_vector.normalized()
-	
+
 	# Set Rotation to Mouse
 	rotate_group.global_rotation = get_global_mouse_position().angle_to_point(global_position)
+
+	# Check for Aiming Input
+	sneaking = Input.is_action_pressed("sneak")
 
 
 func _physics_process(delta: float) -> void:
@@ -28,7 +33,11 @@ func _physics_process(delta: float) -> void:
 	var acc: float = (acceleration if input_vector else drag) * delta
 	var target_speed: float = move_speed
 
+	if sneaking:
+		target_speed *= sneaking_speed_multiplier
+
 	# Apply Acceleration
 	velocity = velocity.move_toward(input_vector * target_speed, acc)
+
 	# Move the Body
 	velocity = move_and_slide(velocity)
