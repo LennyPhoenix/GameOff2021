a98a3eb366e8406ce761b86ffa7b0cb84354ea8a
Lenny Critchley <LennyPhoenixC@gmail.com>
2021-11-04 22:07:19 +0000

    :sparkles: Add basic player movement

---
+++ scenes/Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+export var move_speed := 100.0
+export var acceleration := 1500.0
+export var drag := 1000.0
+
+var input_vector := Vector2.ZERO
+var velocity := Vector2.ZERO
+
+onready var rotate_group: Node2D = $Rotate
+
+
+func _process(_delta: float) -> void:
+	# Get Movement Input
+	input_vector = Vector2.ZERO
+	input_vector.x += Input.get_action_strength("right")
+	input_vector.x -= Input.get_action_strength("left")
+	input_vector.y -= Input.get_action_strength("up")
+	input_vector.y += Input.get_action_strength("down")
+	input_vector = input_vector.normalized()
+	
+	# Set Rotation to Mouse
+	rotate_group.global_rotation = get_global_mouse_position().angle_to_point(global_position)
+
+
+func _physics_process(delta: float) -> void:
+	# Get Acceleration and Target Speed
+	var acc: float = (acceleration if input_vector else drag) * delta
+	var target_speed: float = move_speed
+
+	# Apply Acceleration
+	velocity = velocity.move_toward(input_vector * target_speed, acc)
+	# Move the Body
+	velocity = move_and_slide(velocity)
