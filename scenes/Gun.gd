dbd8046c86a5a14bbce03faedd08738b6d27acb4
Lenny Critchley <LennyPhoenixC@gmail.com>
2021-11-08 22:57:32 +0000

    :recycle: Implement state machine for player and gun

--- scenes/Gun.gd
+++ scenes/Gun.gd
@@ -1,8 +1,17 @@
 class_name Gun
 extends Node2D
 
-signal fired()
-signal reloaded()
+# TODO: Use target_state system along with a get_current_state method,
+#       for each state, have some kind of requirement or a method to update weights and run through
+#       each every _process.
+
+enum State {
+	STOWED,
+	READY,
+	AIMING,
+	RELOADING,
+	ADJUSTING,
+}
 
 # Bullet
 export var bullet_scene: PackedScene = preload("res://scenes/Bullet.tscn")
@@ -12,6 +21,8 @@ export var automatic := true
 export var aim_in_time := 0.3
 export var aim_out_time := 0.1
 export var reload_time := 1.2
+export var stow_time := 0.2
+export var unstown_time := 0.3
 # Spread
 export var hip_spread := 40.0
 export var aiming_spread := 8.0
@@ -27,34 +38,35 @@ export var reload_fade_time := 0.1
 onready var muzzle: Position2D = $Muzzle
 onready var top_guide: Node2D = $Muzzle/TopGuide
 onready var bottom_guide: Node2D = $Muzzle/BottomGuide
+onready var ammo_label: Label = $UI/Ammo
 
+var target_state: int = State.READY
 var aiming_weight := 0.0
-var reloading_fade := 0.0
-var reloading := false
+var stowed_weight := 0.0
+var reloading_weight := 0.0
 onready var rounds: int = mag_size
 
 var _cooldown := 0.0
 
 
+func _ready() -> void:
+	update_ammo_label()
+
+
 func _process(delta: float) -> void:
+	_update_states(delta)
+	var current_state = get_current_state()
+
 	var spread: float = lerp(hip_spread, aiming_spread, aiming_weight)
 	var guide_colour: Color = lerp(hip_guide_colour, aiming_guide_colour, aiming_weight)
 
-	var has_rounds: bool = (
-		not reloading
-		and (mag_size == 0 or rounds > 0)
-	)
-	var shooting: bool = (
-		Input.is_action_pressed("shoot")
+	var has_rounds: bool = current_state != State.RELOADING and (mag_size == 0 or rounds > 0)
+	var shooting: bool = current_state in [State.READY, State.AIMING] and (
+		(Input.is_action_pressed("shoot") and automatic)
 		or Input.is_action_just_pressed("shoot")
 	)
 
-	if reloading:
-		reloading_fade += delta / reload_fade_time
-	else:
-		reloading_fade -= delta / reload_fade_time
-	reloading_fade = clamp(reloading_fade, 0, 1)
-	guide_colour = lerp(guide_colour, Color(1.0, 1.0, 1.0, 0.0), reloading_fade)
+	guide_colour = lerp(guide_colour, Color(1.0, 1.0, 1.0, 0.0), reloading_weight)
 
 	_cooldown -= delta
 
@@ -75,15 +87,53 @@ func _process(delta: float) -> void:
 			if mag_size > 0:
 				rounds -= 1
 			_cooldown = 1 / rounds_per_second
-			emit_signal("fired")
+
+			update_ammo_label()
 		else:
 			reload()
 
 
+func _update_states(delta: float) -> void:
+	if target_state == State.RELOADING:
+		reloading_weight += delta / reload_fade_time
+	else:
+		reloading_weight -= delta / reload_fade_time
+
+		if target_state == State.AIMING:
+			aiming_weight += delta / aim_in_time
+		else:
+			aiming_weight -= delta / aim_out_time
+
+	aiming_weight = clamp(aiming_weight, 0, 1)
+	reloading_weight = clamp(reloading_weight, 0, 1)
+
+
+func get_current_state() -> int:
+	if reloading_weight > 0:
+		return State.RELOADING
+	elif stowed_weight == 1:
+		return State.STOWED
+	elif aiming_weight == 1:
+		return State.AIMING
+	elif reloading_weight + stowed_weight + aiming_weight > 0:
+		return State.ADJUSTING
+	else:
+		return State.READY
+
+
 func reload() -> void:
-	if not reloading and mag_size > 0 and rounds < mag_size:
-		reloading = true
+	var current_state = get_current_state()
+
+	if not current_state in [State.RELOADING, State.STOWED] and mag_size > 0 and rounds < mag_size:
+		var prev_state = current_state
+		target_state = State.RELOADING
 		yield(get_tree().create_timer(reload_time), "timeout")
 		rounds = mag_size
-		reloading = false
-		emit_signal("reloaded")
+		target_state = prev_state
+
+		update_ammo_label()
+
+
+func update_ammo_label() -> void:
+	ammo_label.visible = mag_size > 0
+	ammo_label.text = "%d / %d" % [rounds, mag_size]
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,53 +1,59 @@
 extends KinematicBody2D
 
+enum State {
+	IDLE,
+	WALKING,
+	SNEAKING,
+	SPRINTING,
+	IN_ACTION,
+}
+
 export var move_speed := 80.0
 export var acceleration := 1500.0
 export var drag := 1000.0
 export var sneaking_speed_multiplier := 0.4
+export var sprinting_speed_multiplier := 1.8
 
-var sneaking := false
+var state: int = State.IDLE
 var input_vector := Vector2.ZERO
 var velocity := Vector2.ZERO
+var stow_weight := 0.0
 
 onready var rotate_group: Node2D = $Rotate
 onready var gun: Gun = $Rotate/Gun
 onready var fov_light: Light2D = $Lights/Vision/FOV
-onready var mag_size: Label = $UI/MagSize
-
-
-func _ready() -> void:
-	update_mag_size()
-
-
-func _on_Gun_fired() -> void:
-	update_mag_size()
-
-
-func _on_Gun_reloaded() -> void:
-	update_mag_size()
 
 
 func _process(_delta: float) -> void:
 	# Get Movement Input
-	input_vector = Vector2.ZERO
-	input_vector.x += Input.get_action_strength("right")
-	input_vector.x -= Input.get_action_strength("left")
-	input_vector.y -= Input.get_action_strength("up")
-	input_vector.y += Input.get_action_strength("down")
-	input_vector = input_vector.normalized()
+	input_vector = Input.get_vector("left", "right", "up", "down")
 
 	# Set Rotation to Mouse
 	rotate_group.global_rotation = get_global_mouse_position().angle_to_point(global_position)
-
-	# Check for Aiming Input
-	sneaking = Input.is_action_pressed("sneak")
 
 	# Check for Reload
 	if Input.is_action_just_pressed("reload"):
 		gun.reload()
 
+	# Input
+	if gun.get_current_state() == Gun.State.RELOADING:
+		state = State.IN_ACTION
+	elif Input.is_action_pressed("sneak"):
+		state = State.SNEAKING
+	elif input_vector == Vector2.ZERO:
+		state = State.IDLE
+	elif Input.is_action_pressed("sprint"):
+		state = State.SPRINTING
+	else:
+		state = State.WALKING
+
+	if state == State.SNEAKING and gun.get_current_state() == Gun.State.READY:
+		gun.target_state = Gun.State.AIMING
+	elif state != State.SNEAKING and gun.get_current_state() == Gun.State.AIMING:
+		gun.target_state = Gun.State.READY
+
 	# Lighting
-	fov_light.texture_scale = lerp(1, 0.15, gun.reloading_fade)
+	fov_light.texture_scale = lerp(1, 0.15, gun.reloading_weight)
 
 
 func _physics_process(delta: float) -> void:
@@ -55,23 +61,16 @@ func _physics_process(delta: float) -> void:
 	var acc: float = (acceleration if input_vector else drag) * delta
 	var target_speed: float = move_speed
 
-	if gun.reloading:
+	# Reloading
+	if gun.get_current_state() == Gun.State.RELOADING:
 		target_speed *= gun.reload_speed_modifier
+
 	# Update Sneaking/Aiming
-	elif sneaking:
+	elif state == State.SNEAKING:
 		target_speed *= sneaking_speed_multiplier
-		gun.aiming_weight += delta / gun.aim_in_time
-	else:
-		gun.aiming_weight -= delta / gun.aim_out_time
-	gun.aiming_weight = clamp(gun.aiming_weight, 0, 1)
 
 	# Apply Acceleration
 	velocity = velocity.move_toward(input_vector * target_speed, acc)
 
 	# Move the Body
 	velocity = move_and_slide(velocity)
-
-
-func update_mag_size() -> void:
-	mag_size.visible = gun.mag_size > 0
-	mag_size.text = "%d / %d" % [gun.rounds, gun.mag_size]
