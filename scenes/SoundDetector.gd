8c3003b803eeed53925b8d2fc2e2c91e7ff64f70
Lenny Critchley <LennyPhoenixC@gmail.com>
2021-11-10 23:58:43 +0000

    :sparkles: Implement "heavy" sounds (aka blocking)

--- scenes/SoundDetector.gd
+++ scenes/SoundDetector.gd
@@ -1,9 +1,19 @@
-tool
 extends Node2D
 
-export var radius := 140.0
+export var radius := 140.0 setget set_radius
+
+var blocking_timer := 0.0
+
+
+func _process(delta: float) -> void:
+	blocking_timer -= delta
+	material.set_shader_param("disruption_time", blocking_timer)
 
 
 func _draw() -> void:
-	if Engine.editor_hint:
-		draw_circle(Vector2.ZERO, radius, Color(1, 1, 1, 0.1))
+	draw_circle(Vector2.ZERO, radius, Color(1, 1, 1, 1 - blocking_timer))
+
+
+func set_radius(new_radius: float) -> void:
+	radius = new_radius
+	update()
--- scenes/SoundEmitter.gd
+++ scenes/SoundEmitter.gd
@@ -3,7 +3,10 @@ extends Node2D
 onready var particles: CPUParticles2D = $Particles
 
 
-func emit_sound() -> void:
+func emit_sound(blocking: bool = false, blocking_length: float = 0.4) -> void:
 	for detector in get_tree().get_nodes_in_group("SoundDetector"):
 		if global_position.distance_to(detector.global_position) < detector.radius:
-			particles.restart()
+			if blocking:
+				detector.blocking_timer = blocking_length
+			else:
+				particles.restart()
